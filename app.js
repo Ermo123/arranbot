080f0bfc8f81522af5fad4374428d9a93f785687
Leon <44817515+Ermo123@users.noreply.github.com>
2021-11-06 20:10:54 -0600

    luis
    crespo

--- app.js
+++ app.js
@@ -1,47 +1,17 @@
 var port = process.env.PORT || 3000
     http = require('http')
-    fs = require('fs')
+    //fs = require('fs')
     //html = fs.readFileSync('Home.html');
-    app.get('/*', function(req,res) {
-        data= fs.readFile('/' + req.url,   function (err, data) {
-        res.setHeader('Content-Type', 'text/html');
-        res.send(data);
-    })});
+    const express = require('express');
+    const cors = require('cors');
+    const app = express();
+    app.use(express.static(__dirname+'/home'))
+    app.use('/commands',express.static(__dirname+'/home/commands'))
+    app.use('/feedback',express.static(__dirname+'/home/feedback'))
 
-
-
-
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
-
-var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
-
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
-});
 
 // Listen on port 3000, IP defaults to 127.0.0.1
 server.listen(port);
 
-// Put a friendly message on the terminal
+// Put a bad ass message on the terminal
 console.log('Server running at http://127.0.0.1:' + port + '/');
