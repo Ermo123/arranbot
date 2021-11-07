39becba925d0a64225481d14254a623a565df787
Leon <44817515+Ermo123@users.noreply.github.com>
2021-11-06 20:15:55 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var port = process.env.PORT || 3000
     const express = require('express');
     const cors = require('cors');
     const app = express();
+    app.use(cors());
+    app.use(express.json());
     app.use(express.static(__dirname+'/home'))
     app.use('/commands',express.static(__dirname+'/home/commands'))
     app.use('/feedback',express.static(__dirname+'/home/feedback'))
